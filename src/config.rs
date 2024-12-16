4d077d7643cb6c0734ec3d3d9831b41f5f73ec00
Mai <mai@tilde.team>
2024-12-16 18:30:54 -0300

    refactor some logging into a macro

--- src/config.rs
+++ src/config.rs
@@ -1,18 +1,14 @@
 use std::env;
 
-use crate::utils::set_global;
-use nvim_oxi::api::{self, opts::NotifyOpts, types::LogLevel};
+use crate::utils::{set_global, warning};
+use nvim_oxi::api;
 
 /// Sets up `vim.o` and `vim.g` variables.
 pub fn setup_options() -> Result<(), api::Error> {
     if let Ok(home) = env::var("HOME") {
         set_global!(("undodir", format!("{home}/.cache/nvim/undodir")));
     } else {
-        api::notify(
-            "[vimetal] no $HOME defined, can't set 'undodir'.",
-            LogLevel::Warn,
-            &NotifyOpts::default(),
-        )?;
+        warning!("[vimetal] no $HOME defined, can't set 'undodir'.");
     }
 
     set_global!(
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -10,4 +10,26 @@ macro_rules! set_global {
     }
 }
 
+macro_rules! warning {
+    ( $msg:expr ) => {
+        nvim_oxi::api::notify(
+            $msg,
+            nvim_oxi::api::types::LogLevel::Warn,
+            &nvim_oxi::api::opts::NotifyOpts::default(),
+        ).ok();
+    };
+}
+
+macro_rules! info {
+    ( $msg:expr ) => {
+        nvim_oxi::api::notify(
+            $msg,
+            nvim_oxi::api::types::LogLevel::Info,
+            nvim_oxi::api::opts::NotifyOpts::default(),
+        )
+    };
+}
+
+pub(crate) use info;
 pub(crate) use set_global;
+pub(crate) use warning;
